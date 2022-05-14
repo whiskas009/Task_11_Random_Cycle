2591a7cba18a9095d3e9fb226afcb8ce41705e6b
whiskas009 <76867818+whiskas009@users.noreply.github.com>
2022-05-14 23:39:02 +0300

    #1

---
+++ Task_11_Random_Cycle/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_11_Random_Cycle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomLimit = random.Next(0, 100);
+            int sum = 0;
+
+            for (int termsNumber = 0; termsNumber < randomLimit; termsNumber++)
+            {
+                if (termsNumber % 3 == 0 || termsNumber % 5 == 0)
+                    sum += termsNumber;
+            }
+            
+            Console.WriteLine(sum);
+        }
+    }
+}
